0be99f3bfa8cb862791d424ae9c4f4d22549027a
shiva sharma <shivathebravo@gmail.com>
2016-12-26 11:04:24 -0500

    Added Generic Method

--- CSharpAdvanceConcepts/Program.cs
+++ CSharpAdvanceConcepts/Program.cs
@@ -10,6 +10,30 @@ namespace CSharpAdvanceConcepts
     {
         static void Main(string[] args)
         {
+            var result = new Result<int> {Success = true, Data = 5};
+            var result2 = new Result<string> {Success = false, Data = "Shiva"};
+            var helper = new ResultPrinter();
+
+            helper.Print(result);
+            helper.Print(result2);
+
         }
+
+        public class Result<T>
+        {
+            public bool Success { get; set; }
+            public T Data { get; set; }
+            
+        }
+
+        public class ResultPrinter
+        {
+            public void Print<T>(Result<T> result)
+            {
+                Console.WriteLine(result.Success);
+                Console.WriteLine(result.Data);
+            }
+        }
+
     }
 }
